7b7b2c2e3972bc7d5f293670c8ebd03630ecaf40
Yashwant <yshriol94@gmail.com>
2018-06-03 21:17:32 +0530

    Added accordian to the main table

--- app.js
+++ app.js
@@ -1,25 +1,107 @@
-angular.module("App", ['main']);
-angular.module("main",[]);
+var App = angular.module('App', ['datatables', 'ui.bootstrap']);
 
- angular.module("main")
-    .controller("mainController", ($scope) => {
-        var data = "roger";
-        var name;
-        $scope.name = "hello"
-        $scope.data = "world";
-});
+App.controller('mainController', ['$scope', 'DTOptionsBuilder', 'DTColumnDefBuilder', '$uibModal', '$http',
+    function($scope, DTOptionsBuilder, DTColumnDefBuilder, $uibModal, $http) {
 
-angular.module("main", ['datatables']).controller('WithOptionsCtrl', WithOptionsCtrl);
+        $scope.data = '';
+        $scope.values = '';
+        $scope.selectedRow = 0;
 
-function WithOptionsCtrl(DTOptionsBuilder, DTColumnDefBuilder) {
-    var vm = this;
-    vm.dtOptions = DTOptionsBuilder.newOptions()
-        .withPaginationType('full_numbers')
-        .withDisplayLength(2)
-        .withDOM('pitrfl');
-    vm.dtColumnDefs = [
-        DTColumnDefBuilder.newColumnDef(0),
-        DTColumnDefBuilder.newColumnDef(1).notVisible(),
-        DTColumnDefBuilder.newColumnDef(2).notSortable()
-    ];
-}
+        $http({
+            type: 'GET',
+            url: './data.json',
+            dataType: 'json',
+        }).then((d) => {
+            $scope.data = d.data;
+        });
+
+        $scope.toggleHiddenRow = function(id){
+            $scope.activeRow = id;
+        }; 
+        $scope.dtOptions = DTOptionsBuilder.newOptions()
+            .withPaginationType('full_numbers')
+            .withDisplayLength(2)
+            .withDOM('pitrfl');
+        $scope.dtColumnDefs = [
+            DTColumnDefBuilder.newColumnDef(0),
+            DTColumnDefBuilder.newColumnDef(2).notSortable()
+        ];
+
+        $scope.editModal = (a) => {
+            $('#editModal').modal({});
+            $scope.values = a;
+        }
+    }
+]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// angular.module('App')
+//     .controller('mainController', ['$scope', 'datatables',
+//         function($scope, datatables) {
+
+//         var data = "roger";
+//         var name;
+//         var vm = this;
+//         $scope.name = "hello"
+//         $scope.data = "world";
+//         vm.dtOptions = DTOptionsBuilder.newOptions()
+//             .withPaginationType('full_numbers')
+//             .withDisplayLength(3)
+//             .withDOM('pitrfl');
+//             vm.dtColumnDefs = [
+//             DTColumnDefBuilder.newColumnDef(0),
+//             // DTColumnDefBuilder.newColumnDef(1).notVisible(),
+//             DTColumnDefBuilder.newColumnDef(2).notSortable()
+//         ];
+//         vm.openModel = function() {
+//             console.log(hello)
+//         }
+// }]);
+
+// // angular.module("main", ['datatables']).controller('mainController', mainController);
+
